2cf2394d01347800e27d7a494ceca7ef27f328f9
Raj14y2 <rajashekarkp05@gmail.com>
2024-03-09 14:51:01 +0530

    new

---
+++ src/test/java/Doctor/mm.java
@@ -0,0 +1,13 @@
+package Doctor;
+
+import org.testng.annotations.Test;
+
+public class mm 
+{
+	@Test
+	public void test8()
+	{
+		System.out.println("--execute test8---");
+	}
+
+}
---
+++ src/test/java/Patient/iii.java
@@ -0,0 +1,11 @@
+package Patient;
+
+import org.testng.annotations.Test;
+
+public class iii {
+	@Test
+	public void test8()
+	{
+		System.out.println("--execute test8---");
+	}
+}
