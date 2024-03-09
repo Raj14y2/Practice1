5cf85c1993605f4f4b5d8e6b84d702b43f89ab5a
Raj14y2 <rajashekarkp05@gmail.com>
2024-03-09 13:06:22 +0530

    data is transferred from working directory to local repo

---
+++ src/test/java/Doctor/TestCase2Test.java
@@ -0,0 +1,13 @@
+package Doctor;
+
+import org.testng.annotations.Test;
+
+public class TestCase2Test 
+{
+
+	@Test
+	public void test2()
+	{
+		System.out.println("--executing testscript2---");
+	}
+}
---
+++ src/test/java/Doctor/Testcase1Test.java
@@ -0,0 +1,13 @@
+package Doctor;
+
+import org.testng.annotations.Test;
+
+public class Testcase1Test 
+{
+
+	@Test
+	public void test1()
+	{
+		System.out.println("--executing testscript one---");
+	}
+}
